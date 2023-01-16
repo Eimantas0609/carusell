39a8beed40fac047d16e8be4e3a9693a3881643a
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2023-01-16 17:44:19 +0200

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,10 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { Container } from "react-bootstrap";
 import "./App.scss";
 
-// class Slider extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			autoplay: false,
-// 			slide: 0,
-// 		};
-// 	}
-
-// 	changeSlide = (i) => {
-// 		this.setState(({ slide }) => ({
-// 			slide: slide + i,
-// 		}));
-// 	};
-
-// 	toggleAutoplay = () => {
-// 		this.setState(({ autoplay }) => ({
-// 			autoplay: !autoplay,
-// 		}));
-// 	};
-
-// 	render() {
-// 		return (
-// 			<Container>
-// 				<div className="slider w-50 m-auto">
-// 					<img
-// 						className="d-block w-100"
-// 						src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-// 						alt="slide"
-// 					/>
-// 					<div className="text-center mt-5">
-// 						Active slide {this.state.slide} <br />{" "}
-// 						{this.state.autoplay ? "auto" : null}
-// 					</div>
-// 					<div className="buttons mt-3">
-// 						<button
-// 							className="btn btn-primary me-2"
-// 							onClick={() => this.changeSlide(-1)}>
-// 							-1
-// 						</button>
-// 						<button
-// 							className="btn btn-primary me-2"
-// 							onClick={() => this.changeSlide(1)}>
-// 							+1
-// 						</button>
-// 						<button
-// 							className="btn btn-primary me-2"
-// 							onClick={this.toggleAutoplay}>
-// 							toggle autoplay
-// 						</button>
-// 					</div>
-// 				</div>
-// 			</Container>
-// 		);
-// 	}
-// }
+const countTotal = (num) => {
+	return num + 10;
+};
 
 const Slider = (props) => {
 	const [slide, setSlide] = useState(0);
@@ -70,27 +17,6 @@ const Slider = (props) => {
 			"https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
 		];
 	}, []);
-	// const [state, setState] = useState({ slide: 0, autoplay: false });
-
-	// function changeSlide(i) {
-	// 	setState((state) => ({ ...state, slide: state.slide + 1 }));
-	// }
-
-	// function toggleAutoplay() {
-	// 	setState((state) => ({ ...state, autoplay: !state.autoplay }));
-	// }
-	function logging() {
-		console.log("log!");
-	}
-
-	useEffect(() => {
-		document.title = `Slide: ${slide}`;
-		window.addEventListener("click", logging);
-
-		return () => {
-			window.removeEventListener("click", logging);
-		};
-	}, [slide]);
 
 	function changeSlide(i) {
 		setSlide((slide) => slide + i);
@@ -99,6 +25,10 @@ const Slider = (props) => {
 	function toggleAutoplay() {
 		setAutoplay((autoplay) => !autoplay);
 	}
+
+	const total = useMemo(() => {
+		return countTotal(slide);
+	}, [slide]);
 
 	return (
 		<Container>
@@ -113,6 +43,7 @@ const Slider = (props) => {
 				<div className="text-center mt-5">
 					Active slide {slide} <br /> {autoplay ? "auto" : null}
 				</div>
+				<div className="text-center mt-5">Total slides: {total}</div>
 				<div className="buttons mt-3">
 					<button
 						className="btn btn-primary me-2"
